a1f1cc4d718291e9684bbcc13e993f1cffc0a06a
Adrian <adricardo679@gmail.com>
2026-04-01 10:50:39 -0700

    okay i have gone all the way to number 6

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,10 +1,41 @@
-import React from "react";
+// src/MyApp.jsx
+import React, { useState } from "react";
 import Table from "./Table";
+import "./main.css";
 
 function MyApp() {
+  const [characters, setCharacters] = useState([
+    {
+      name: "Charlie",
+      job: "Janitor"
+    },
+    {
+      name: "Mac",
+      job: "Bouncer"
+    },
+    {
+      name: "Dee",
+      job: "Aspiring actress"
+    },
+    {
+      name: "Dennis",
+      job: "Bartender"
+    }
+  ]);
+
+  function removeOneCharacter(index) {
+    const updated = characters.filter((character, i) => {
+      return i !== index;
+    });
+    setCharacters(updated);
+  }
+
   return (
     <div className="container">
-      <Table />
+      <Table
+        characterData={characters}
+        removeCharacter={removeOneCharacter}
+      />
     </div>
   );
 }
