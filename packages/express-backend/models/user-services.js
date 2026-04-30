231ddba297729fc49bdd8739f0e94f9f1a95e6c0
Adrian <adricardo679@gmail.com>
2026-04-30 11:21:38 -0700

    updated teh script so now it should all work

--- packages/express-backend/models/user-services.js
+++ packages/express-backend/models/user-services.js
@@ -5,8 +5,7 @@ mongoose.set("debug", true);
 
 mongoose
   .connect("mongodb://localhost:27017/users", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    
   })
   .catch((error) => console.log(error));
 
