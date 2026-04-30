ac2e5d8a0d45b306a9726dd00c176aa0ceb7592b
Adrian <adricardo679@gmail.com>
2026-04-30 10:57:13 -0700

    done with step 3 , changed teh backend.js to follow the monogose queries and pasted teh user-services.js and users.js from the repo in the assignemnt,

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,93 +1,79 @@
-// backend.js
 import express from "express";
 import cors from "cors";
+import userServices from "./models/user-services.js";
+
 const app = express();
 app.use(cors());
 const port = 8000;
-const users = {
-  users_list: [
-    {
-      id: "xyz789",
-      name: "Charlie",
-      job: "Janitor"
-    },
-    {
-      id: "abc123",
-      name: "Mac",
-      job: "Bouncer"
-    },
-    {
-      id: "ppp222",
-      name: "Mac",
-      job: "Professor"
-    },
-    {
-      id: "yat999",
-      name: "Dee",
-      job: "Aspiring actress"
-    },
-    {
-      id: "zap555",
-      name: "Dennis",
-      job: "Bartender"
-    }
-  ]
-};
+
 app.use(express.json());
 
-const findUserByNameAndJob = (name, job) => {
-  return users["users_list"].filter(
-    (user) => user["name"] === name && user["job"] === job
-  );
-};
-
+// GET all users or filter by name/job
 app.get("/users", (req, res) => {
   const name = req.query.name;
   const job = req.query.job;
-  if (name !== undefined && job !== undefined) {
-    let result = findUserByNameAndJob(name, job);
-    result = { users_list: result };
-    res.send(result);
-  } else if (name !== undefined) {
-    let result = findUserByName(name);
-    result = { users_list: result };
-    res.send(result);
-  } else {
-    res.send(users);
-  }
+
+  userServices
+    .getUsers(name, job)
+    .then((users) => {
+      res.send({ users_list: users });
+    })
+    .catch((error) => {
+      res.status(500).send({ error: error.message });
+    });
 });
 
+// GET user by ID
 app.get("/users/:id", (req, res) => {
-  const id = req.params["id"];
-  let result = findUserById(id);
-  if (result === undefined) {
-    res.status(404).send("Resource not found.");
-  } else {
-    res.send(result);
-  }
+  const id = req.params.id;
+
+  userServices
+    .findUserById(id)
+    .then((user) => {
+      if (user === null) {
+        res.status(404).send("Resource not found.");
+      } else {
+        res.send(user);
+      }
+    })
+    .catch((error) => {
+      res.status(500).send({ error: error.message });
+    });
 });
 
-function generateRandomId() {
-  return Math.floor(Math.random() * 1000000).toString();
-}
-
+// POST new user
 app.post("/users", (req, res) => {
   const newUser = req.body;
-  newUser.id = generateRandomId();
-  users["users_list"].push(newUser);
-  res.status(201).send(newUser);
+
+  userServices
+    .addUser(newUser)
+    .then((user) => {
+      res.status(201).send(user);
+    })
+    .catch((error) => {
+      res.status(500).send({ error: error.message });
+    });
 });
 
+// DELETE user by ID
 app.delete("/users/:id", (req, res) => {
-  const userId = req.params["id"];
-  const index = users["users_list"].findIndex(user => user.id == userId);
-  
-  if (index !== -1) {
-    users["users_list"].splice(index, 1);
-    res.status(204).send();
-  } else {
-    res.status(404).send();
-  }
+  const userId = req.params.id;
+
+  userServices
+    .findUserById(userId)
+    .then((user) => {
+      if (user === null) {
+        res.status(404).send();
+      } else {
+        return userServices.deleteUser(userId); // You'll need to add this function
+      }
+    })
+    .then(() => {
+      res.status(204).send();
+    })
+    .catch((error) => {
+      res.status(500).send({ error: error.message });
+    });
 });
 
 app.listen(port, () => {
